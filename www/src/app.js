ee21b51bb3edd18e6de55fd10a170463e329a6a3
spsseano <spsseano@gmail.com>
2015-02-28 10:10:48 -0800

    seperated asteroids into sep class

--- www/src/app.js
+++ www/src/app.js
@@ -405,51 +405,7 @@ var gameLayer = cc.Layer.extend({
 });
 
 
-var largeAsteroid = cc.Sprite.extend({
-    ctor:function(arg) {
-      this._super(asset.Asteroid_png);
-      this.attr({
-            name : "lrg",
-            x: cc.winSize.width / 2,
-            y: cc.winSize.height / 2,
-            scale: 0.5,
-            rotation: 0,
-            xVelocity : 0,
-            yVelocity : 0,
-            radius : (this.getContentSize().width / 4),
-      });
-    }
-});
 
-var mediumAsteroid = cc.Sprite.extend({
-    ctor:function(arg) {
-      this._super(asset.Asteroid_png);
-      this.attr({
-            name : "med",
-            x: cc.winSize.width / 2,
-            y: cc.winSize.height / 2,
-            scale: 0.25,
-            rotation: 0,
-            xVelocity : 0,
-            yVelocity : 0,
-      });
-    }
-});
-
-var smallAsteroid = cc.Sprite.extend({
-    ctor:function(arg) {
-      this._super(asset.Asteroid_png);
-      this.attr({
-            name : "sml",
-            x: cc.winSize.width / 2,
-            y: cc.winSize.height / 2,
-            scale: 0.125,
-            rotation: 0,
-            xVelocity : 0,
-            yVelocity : 0,
-      });
-    }
-});
 
 var ufo = cc.Sprite.extend({
     ctor:function(arg) {
